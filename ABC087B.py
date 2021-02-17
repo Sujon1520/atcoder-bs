6cd0b32bd974fe73ba7b226c1b3ed2c55690f665
Sujon1520 <ap19062@azec.ac.jp>
2021-02-17 14:54:18 +0900

    ABC087Bcoins

--- ABC087B.py
+++ ABC087B.py
@@ -7,10 +7,8 @@ x = int(input())  #Total amount
 num_combination = 0
 for n500 in range(0,a+1):
     for n100 in range(0, b+1):
-        for n50 in range(0,c+1):
-            amount = 500*n500+100*n100+50*n50
-            # print(n500, n100, n50, amount)
-            if amount == x:
-                num_combination += 1
+        n50 = (x - n500*500 - n100*100) // 50
+        if n50 <= c and n50>=0:
+            num_combination += 1
 
 print(num_combination)
